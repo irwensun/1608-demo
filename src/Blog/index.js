85e9e601531917cb7a6a1f2c3078bd32b49900d6
irwensun <1074266737@qq.com>
2016-12-03 11:08:24 +0800

    blog

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import BlogCard from "../component/BlogCard";
 class Blog extends React.Component {
   render () {
+    let cards=data.map((item,i) => <BlogCard {...item} key={i} />)
     return(
-      <div>
-      blog页面
+      <div className="blog-wrap">
+      {cards}
       </div>
     )
   }
