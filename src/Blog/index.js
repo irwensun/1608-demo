590be75602f235e1006f08ffff496f2981ef35af
irwensun <1074266737@qq.com>
2016-12-04 09:56:47 +0800

    post1

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -14,7 +14,7 @@ class Blog extends React.Component {
 
   }
   componentDidMount(){
-    axios.get("https://raw.githubusercontent.com/irwensun/1608-demo/master/data/blogcard.json")
+    axios.get("https://raw.githubusercontent.com/irwensun/1608-demo/master/data/blogcard.json?hjbjjhb")
          .then( res=> this.setState({data:res.data,wait:false}))
   }
   render () {
